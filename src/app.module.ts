68cbf924ceb3bd6876ea612196391ca35210da9a
Eric Almendral <eralmendral@gmail.com>
2022-11-21 17:37:14 +0800

    setup pusher

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,33 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { TwilioModule } from 'nestjs-twilio';
+import { TwilioModule } from 'nestjs-twilio'
+import { PusherModule } from 'nestjs-pusher'
+
 
 @Module({
-    imports: [ConfigModule.forRoot(),
-        TwilioModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (cfg: ConfigService) => ({
-                accountSid: cfg.get('TWILIO_ACCOUNT_SID'),
-                authToken:  cfg.get('TWILIO_AUTH_TOKEN')
-            }),
-            inject: [ConfigService]
-          }),
+    imports: [
+
+        // Config Module
+        ConfigModule.forRoot(),
+
+        // Twilio Module
+        TwilioModule.forRoot({
+            accountSid: process.env.TWILIO_ACCOUNT_SID,
+            authToken: process.env.TWILIO_AUTH_TOKEN,
+        }),
+
+        // Pusher Module
+        PusherModule.forRoot({
+            key: process.env.PUSHER_APP_KEY,
+            appId: process.env.PUSHER_APP_ID,
+            secret: process.env.PUSHER_APP_SECRET,
+            cluster: process.env.PUSHER_APP_CLUSTER
+        }, {
+            limit: 4000, //4mb
+            enabled: true
+        }, true)
+
     ],
     controllers: [AppController],
     providers: [AppService],
