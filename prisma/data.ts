8d0ac4cc115f73f4189c6903bd02cc64dce920fc
Eric Almendral <eralmendral@gmail.com>
2022-11-30 05:09:42 +0800

    prisma schema , seeder

---
+++ prisma/data.ts
@@ -0,0 +1,213 @@
+export const policiesData = [
+    {
+        "id": 2553,
+        "name": "Policy create test updateddd",
+        "category_id": 163
+    },
+    {
+        "id": 2554,
+        "name": "Tustin@JPOApplianceRepair.com",
+        "category_id": 163
+    },
+    {
+        "id": 2555,
+        "name": "Mon thru Fri  7:30am to 5:00pm",
+        "category_id": 163
+    },
+    {
+        "id": 2556,
+        "name": "Saturday 8:00am thru noon",
+        "category_id": 163
+    },
+    {
+        "id": 2557,
+        "name": "Visa, MasterCard, Discover, American Express, Check, cash",
+        "category_id": 163
+    },
+    {
+        "id": 2559,
+        "name": "MSV +P+L+T",
+        "category_id": 163
+    },
+    {
+        "id": 2560,
+        "name": "We go to areas outside of the Tustin-metro for an additional amount of between $10 and $30 extra.   ",
+        "category_id": 163
+    },
+    {
+        "id": 2561,
+        "name": "Second appliance at the same time, $50.00.",
+        "category_id": 163
+    },
+    {
+        "id": 2562,
+        "name": "Visa, MasterCard, Discover, American Express, Check, cash",
+        "category_id": 163
+    },
+    {
+        "id": 2563,
+        "name": "COD:  1 Year  Labor and 1 Year on Parts",
+        "category_id": 163
+    },
+    {
+        "id": 2564,
+        "name": "See spreadsheet",
+        "category_id": 162
+    },
+    {
+        "id": 2565,
+        "name": "See spreadsheet",
+        "category_id": 163
+    },
+    {
+        "id": 2566,
+        "name": " Warranty:  90 Days Labor and 1 Year Parts",
+        "category_id": 164
+    },
+    {
+        "id": 2567,
+        "name": "Laundry, Kitchen= gas and electric,   We do not service disposals or commercial equipment. ",
+        "category_id": 165
+    },
+    {
+        "id": 2568,
+        "name": "NO INSTALLS",
+        "category_id": 165
+    },
+    {
+        "id": 2569,
+        "name": "Cancellation policy – 24 hrs (does not enforce)",
+        "category_id": 166
+    },
+    {
+        "id": 2570,
+        "name": "Offer ONLY All-Day, take AM or PM if customer insists but PREFERENCE ONLY",
+        "category_id": 166
+    },
+    {
+        "id": 2571,
+        "name": "EMAIL REQUIRED FOR ALL CUSTOMERS if possible",
+        "category_id": 166
+    },
+    {
+        "id": 2572,
+        "name": "DO NOT CREATE UIS – just put model/serial in top comment box",
+        "category_id": 166
+    },
+    {
+        "id": 2573,
+        "name": "Reminder Calls – via email only 5:00pm daily w/2 hr time window",
+        "category_id": 166
+    }
+]
+
+
+export const policyCategoryData = [
+    {
+        "id": 162,
+        "name": "Brands In Warranty123567",
+        "client_id": 9
+    },
+    {
+        "id": 163,
+        "name": "Brands Out Of Warranty22",
+        "client_id": 38
+    },
+    {
+        "id": 164,
+        "name": "Warranty/Recall Period",
+        "client_id": 38
+    },
+    {
+        "id": 165,
+        "name": "Products Covered",
+        "client_id": 38
+    },
+    {
+        "id": 166,
+        "name": "Policies & Procedures",
+        "client_id": 38
+    },
+    {
+        "id": 167,
+        "name": "Contacts",
+        "client_id": 38
+    },
+    {
+        "id": 176,
+        "name": "Office Information / Hours",
+        "client_id": 9
+    },
+    {
+        "id": 177,
+        "name": "Rates / Payment",
+        "client_id": 9
+    },
+    {
+        "id": 178,
+        "name": "Brands In Warranty",
+        "client_id": 9
+    },
+    {
+        "id": 179,
+        "name": "Brands Out Of Warranty",
+        "client_id": 9
+    },
+    {
+        "id": 180,
+        "name": "Warranty / Recall Period",
+        "client_id": 9
+    },
+    {
+        "id": 181,
+        "name": "Products Serviced",
+        "client_id": 9
+    },
+    {
+        "id": 182,
+        "name": "Policies & Procedures",
+        "client_id": 9
+    },
+    {
+        "id": 183,
+        "name": "Contacts",
+        "client_id": 9
+    },
+    {
+        "id": 583,
+        "name": "Quick Reference",
+        "client_id": 67
+    },
+    {
+        "id": 585,
+        "name": "Brands In Warranty",
+        "client_id": 67
+    },
+    {
+        "id": 586,
+        "name": "Brands Out of Warranty",
+        "client_id": 67
+    },
+    {
+        "id": 587,
+        "name": "Office Information / Hours",
+        "client_id": 67
+    },
+    {
+        "id": 588,
+        "name": "Parts Procedures",
+        "client_id": 67
+    },
+    {
+        "id": 589,
+        "name": "Rates / Payment",
+        "client_id": 67
+    }
+]
+
+export const clientsData = [
+    { id: 38, name: "JPO Appliance" },
+    { id: 67, name: "JPO Corp" },
+    { id: 9, name: "Flamngo Appliance" },
+    { id: 95, name: "AT Appliance Sacramento" }
+]
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,134 +1,33 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+import { policiesData, policyCategoryData, clientsData } from './data'
+
 
 async function main() {
     seedClient()
-
     setTimeout(() => {
         seedPolicies()
     }, 3000)
 }
 
 function seedClient() {
-    let clientData = [
-        { id: 38, name: "JPO Appliance" },
-        { id: 67, name: "JPO Corp" },
-        { id: 9, name: "Flamngo Appliance" },
-        { id: 95, name: "AT Appliance Sacramento" }
-    ]
-
-    clientData.forEach(async (data) => {
+    clientsData.forEach(async (data) => {
         await prisma.client.upsert({ where: { id: data.id }, update: {}, create: data })
     })
 }
 
 function seedPolicies() {
-    let policyCategoryData = [
-        {
-            "id": 162,
-            "name": "Brands In Warranty123567",
-            "client_id": 9
-        },
-        {
-            "id": 163,
-            "name": "Brands Out Of Warranty22",
-            "client_id": 38
-        },
-        {
-            "id": 164,
-            "name": "Warranty/Recall Period",
-            "client_id": 38
-        },
-        {
-            "id": 165,
-            "name": "Products Covered",
-            "client_id": 38
-        },
-        {
-            "id": 166,
-            "name": "Policies & Procedures",
-            "client_id": 38
-        },
-        {
-            "id": 167,
-            "name": "Contacts",
-            "client_id": 38
-        },
-        {
-            "id": 176,
-            "name": "Office Information / Hours",
-            "client_id": 9
-        },
-        {
-            "id": 177,
-            "name": "Rates / Payment",
-            "client_id": 9
-        },
-        {
-            "id": 178,
-            "name": "Brands In Warranty",
-            "client_id": 9
-        },
-        {
-            "id": 179,
-            "name": "Brands Out Of Warranty",
-            "client_id": 9
-        },
-        {
-            "id": 180,
-            "name": "Warranty / Recall Period",
-            "client_id": 9
-        },
-        {
-            "id": 181,
-            "name": "Products Serviced",
-            "client_id": 9
-        },
-        {
-            "id": 182,
-            "name": "Policies & Procedures",
-            "client_id": 9
-        },
-        {
-            "id": 183,
-            "name": "Contacts",
-            "client_id": 9
-        },
-        {
-            "id": 583,
-            "name": "Quick Reference",
-            "client_id": 67
-        },
-        {
-            "id": 585,
-            "name": "Brands In Warranty",
-            "client_id": 67
-        },
-        {
-            "id": 586,
-            "name": "Brands Out of Warranty",
-            "client_id": 67
-        },
-        {
-            "id": 587,
-            "name": "Office Information / Hours",
-            "client_id": 67
-        },
-        {
-            "id": 588,
-            "name": "Parts Procedures",
-            "client_id": 67
-        },
-        {
-            "id": 589,
-            "name": "Rates / Payment",
-            "client_id": 67
-        }
-    ]
-
+    // Seed policy categories.
     policyCategoryData.forEach(async (data) => {
         await prisma.policyCategory.upsert({ where: { id: data.id }, update: {}, create: data })
     })
+
+    // Seed policies.
+    setTimeout(() => {
+        policiesData.forEach(async (data) => {
+            await prisma.policy.upsert({ where: { id: data.id }, update: {}, create: data })
+        })
+    }, 3000)
 }
 
 main()
