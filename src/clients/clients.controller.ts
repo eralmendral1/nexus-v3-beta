9a871121867f24b470af844dc73ce4b255ab9a4c
Eric Almendral <eralmendral@gmail.com>
2022-11-30 03:56:33 +0800

    add guard clients endpoint

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common'
 import { ClientsService } from './clients.service'
 import { CreateClientDto } from './dto/create-client.dto'
 import { UpdateClientDto } from './dto/update-client.dto'
 import { ApiTags } from '@nestjs/swagger'
+import { AccessTokenGuard } from '../auth/auth.guard'
 
 @Controller('clients')
 @ApiTags('Clients')
+@UseGuards(AccessTokenGuard)
 export class ClientsController {
     constructor(private readonly clientsService: ClientsService) { }
 
