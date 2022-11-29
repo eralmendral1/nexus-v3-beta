8fc24c4adcbc980728f957b2d2d4c4f26f671127
Eric Almendral <eralmendral@gmail.com>
2022-11-30 04:40:28 +0800

    prisma schema, seeders

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+    seedClient()
 
-    // Seed Clients
+    setTimeout(() => {
+        seedPolicies()
+    }, 3000)
+}
+
+function seedClient() {
     let clientData = [
         { id: 38, name: "JPO Appliance" },
         { id: 67, name: "JPO Corp" },
@@ -12,11 +18,119 @@ async function main() {
     ]
 
     clientData.forEach(async (data) => {
-        await prisma.client.create({ data })
+        await prisma.client.upsert({ where: { id: data.id }, update: {}, create: data })
     })
-
-    // End Seed Clients
 }
+
+function seedPolicies() {
+    let policyCategoryData = [
+        {
+            "id": 162,
+            "name": "Brands In Warranty123567",
+            "client_id": 9
+        },
+        {
+            "id": 163,
+            "name": "Brands Out Of Warranty22",
+            "client_id": 38
+        },
+        {
+            "id": 164,
+            "name": "Warranty/Recall Period",
+            "client_id": 38
+        },
+        {
+            "id": 165,
+            "name": "Products Covered",
+            "client_id": 38
+        },
+        {
+            "id": 166,
+            "name": "Policies & Procedures",
+            "client_id": 38
+        },
+        {
+            "id": 167,
+            "name": "Contacts",
+            "client_id": 38
+        },
+        {
+            "id": 176,
+            "name": "Office Information / Hours",
+            "client_id": 9
+        },
+        {
+            "id": 177,
+            "name": "Rates / Payment",
+            "client_id": 9
+        },
+        {
+            "id": 178,
+            "name": "Brands In Warranty",
+            "client_id": 9
+        },
+        {
+            "id": 179,
+            "name": "Brands Out Of Warranty",
+            "client_id": 9
+        },
+        {
+            "id": 180,
+            "name": "Warranty / Recall Period",
+            "client_id": 9
+        },
+        {
+            "id": 181,
+            "name": "Products Serviced",
+            "client_id": 9
+        },
+        {
+            "id": 182,
+            "name": "Policies & Procedures",
+            "client_id": 9
+        },
+        {
+            "id": 183,
+            "name": "Contacts",
+            "client_id": 9
+        },
+        {
+            "id": 583,
+            "name": "Quick Reference",
+            "client_id": 67
+        },
+        {
+            "id": 585,
+            "name": "Brands In Warranty",
+            "client_id": 67
+        },
+        {
+            "id": 586,
+            "name": "Brands Out of Warranty",
+            "client_id": 67
+        },
+        {
+            "id": 587,
+            "name": "Office Information / Hours",
+            "client_id": 67
+        },
+        {
+            "id": 588,
+            "name": "Parts Procedures",
+            "client_id": 67
+        },
+        {
+            "id": 589,
+            "name": "Rates / Payment",
+            "client_id": 67
+        }
+    ]
+
+    policyCategoryData.forEach(async (data) => {
+        await prisma.policyCategory.upsert({ where: { id: data.id }, update: {}, create: data })
+    })
+}
+
 main()
     .then(async () => {
         await prisma.$disconnect()
