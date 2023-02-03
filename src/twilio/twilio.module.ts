ca5ceee0b52e954859e1dc8ca35d58bdedd8703a
mi_eracle <eralmendral@gmail.com>
2023-02-04 03:40:38 +0800

    code formatting

--- src/twilio/twilio.module.ts
+++ src/twilio/twilio.module.ts
@@ -1,15 +1,13 @@
-import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common'
 import { TWILIO_CLIENT } from './constants'
-
 
 @Module({})
 export class TwilioModule {
     static forRoot(accountSid: string, authToken: string, workspace: string): DynamicModule {
-       
         const twilioClient = require('twilio')(accountSid, authToken).taskrouter.v1.workspaces(workspace)
 
         const twilioProvider: Provider = {
-            provide:  TWILIO_CLIENT,
+            provide: TWILIO_CLIENT,
             useValue: twilioClient
         }
 
