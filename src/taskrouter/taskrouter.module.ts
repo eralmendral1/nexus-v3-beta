eb44e4baf09417141a4c0a15b4b2266ff5f1ead9
mi_eracle <eralmendral@gmail.com>
2023-02-04 04:33:03 +0800

    code formatting imports

--- src/taskrouter/taskrouter.module.ts
+++ src/taskrouter/taskrouter.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common'
 import { ReservationsModule } from './reservations/reservations.module'
 import { WorkflowsModule } from './workflows/workflows.module'
-import { ActivitiesModule } from './activities/activities.module';
-import { TaskrouterService } from './taskrouter.service';
-import { ChannelsModule } from './channels/channels.module';
-import { TasksModule } from './tasks/tasks.module';
-import { TaskrouterController } from './taskrouter.controller';
+import { ActivitiesModule } from './activities/activities.module'
+import { TaskrouterService } from './taskrouter.service'
+import { ChannelsModule } from './channels/channels.module'
+import { TasksModule } from './tasks/tasks.module'
+import { TaskrouterController } from './taskrouter.controller'
+import { WorkersModule } from './workers/workers.module'
 
 @Module({
-    imports: [ReservationsModule, WorkflowsModule, ActivitiesModule, ChannelsModule, TasksModule],
+    imports: [
+        ReservationsModule,
+        WorkflowsModule,
+        ActivitiesModule,
+        ChannelsModule,
+        TasksModule,
+        WorkersModule
+    ],
     providers: [TaskrouterService],
     controllers: [TaskrouterController]
 })
