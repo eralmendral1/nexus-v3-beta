5d2204e9e3ca19d79531a5ad9892d13dbde03140
mi_eracle <eralmendral@gmail.com>
2023-02-11 06:37:55 +0800

    wip; taskrouter handler

--- src/taskrouter/taskrouter.controller.ts
+++ src/taskrouter/taskrouter.controller.ts
@@ -6,8 +6,33 @@ export class TaskrouterController {
     @Post('callback')
     handleTaskrouterCallback(@Body() request: Request) {
         console.log('debug request body:', request)
+
+        const eventType = request['EventType']
+
+        switch (eventType) {
+            case 'task.created':
+                break
+            case 'task.updated':
+                break
+
+            case 'task.canceled':
+                //todo create task  completed record.
+                break
+
+            case 'task.wrapup':
+                break
+
+            case 'task.completed':
+                // todo: create task completed record.
+                break
+
+            case 'task.deleted':
+                break
+
+        }
+
         return {}
-    }   
+    }
 
     @Post('error')
     handleTaskrouterError() {
