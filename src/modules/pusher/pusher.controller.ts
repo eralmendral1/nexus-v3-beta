414f4e6ace46d088b7a043b6513f1b8593ff20a5
mi_eracle <eralmendral@gmail.com>
2023-02-17 03:52:25 +0800

    pusher auth endpoints

--- src/modules/pusher/pusher.controller.ts
+++ src/modules/pusher/pusher.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common'
-import { Request } from 'express'
+import { Controller, Post, Body, Req, Res, } from '@nestjs/common'
+import { Request, Response } from 'express'
+import { PusherService } from 'nestjs-pusher'
 
 @Controller('pusher')
 export class PusherController {
+    constructor(private readonly pusherService: PusherService) { }
 
     @Post('channel-existence')
     handleChannelExistenceCallback(@Body() requestBody: Request) {
@@ -11,7 +13,7 @@ export class PusherController {
 
     @Post('presence')
     handlePresenceCallback(@Body() requestBody: Request) {
-
+        return 1
     }
 
     @Post('client-events')
@@ -21,11 +23,45 @@ export class PusherController {
 
     @Post('subscription-count')
     handleSubscriptionCountCallback() {
-        
+
     }
 
     @Post('cache-channels')
     handleCacheChannelsEvents() {
 
+    }
+
+    // **************************** AUTH ENDPOINTS *********************//
+
+    // handle jwt guard token
+    @Post('auth/user')
+    pusherUserAuth(@Req() req: Request, @Res() res: Response) {
+        const socketId = req.body.socket_id
+
+        const user = {
+            user_id: "1",
+            user_info: {
+                name: "Neo Anderson"
+            }
+        }
+
+        const authResponse = this.pusherService.authenticate(socketId, 'nexus-channel', user)
+        res.send(authResponse)
+    }
+
+    @Post('auth/channel')
+    // pusherChannelAuth() {
+    pusherChannelAuth(@Req() req: Request, @Res() res: Response) {
+        const socketId = req.body.socket_id
+        console.log("🚀 ~ file: pusher.controller.ts:56 ~ pusherChannelAuth ~ socketId", socketId)
+        const channel = req.body.channel_name
+        console.log("🚀 ~ file: pusher.controller.ts:57 ~ pusherChannelAuth ~ channel", channel)
+        
+        // handle jwt guard token
+        const pusher = this.pusherService.getPusherInstance()
+        console.log("🚀 ~ file: pusher.controller.ts:60 ~ pusherChannelAuth ~ pusher", pusher)
+        const authResponse = pusher.authorizeChannel(socketId, channel)
+
+        res.send(authResponse)
     }
 }
