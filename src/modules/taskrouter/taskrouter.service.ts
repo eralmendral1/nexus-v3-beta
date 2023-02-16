3b4b2270119ee77aaa54da8a90820c4dca8eb308
mi_eracle <eralmendral@gmail.com>
2023-02-16 19:19:37 +0800

    taskrouter events model

--- src/modules/taskrouter/taskrouter.service.ts
+++ src/modules/taskrouter/taskrouter.service.ts
@@ -1,7 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { TWILIO_TASKROUTER } from '@/modules/twilio/constants'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class TaskrouterService {
-    constructor(@Inject(TWILIO_TASKROUTER) public readonly twilioTaskrouter) { }
+    constructor(@Inject(TWILIO_TASKROUTER) public readonly twilioTaskrouter, private prisma: PrismaService) { }
+
+    createTaskRouterEvent(eventData): void {
+        let { Sid,  Resource, EventType, EventDescription } = eventData
+
+        this.prisma.taskRouterEvent.create({
+            data: {
+                sid: Sid,
+                resource: Resource,
+                event_type: EventType,
+                event_description: EventDescription,
+                event_data: eventData
+            }
+        })
+    }
 }
